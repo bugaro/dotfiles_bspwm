1ba3f67195637bdf8b2aa78d4713d0a01827bb38
Roman Ivanov <varejy@gmail.com>
2021-06-07 20:48:23 +0300

    Update autostart.sh

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -12,34 +12,37 @@ function run {
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
 
 $HOME/.config/polybar/launch.sh &
 
-#change your keyboard if you need it
 setxkbmap -layout "us,ru" -option "grp:rctrl_rshift_toggle"
+
+#change your keyboard if you need it
+#setxkbmap -layout be
 
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
-#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #feh --randomize --bg-fill ~/Képek/*
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
-#dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+# dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
-#conky -c $HOME/.config/bspwm/system-overview &
-#run variety &
-run nm-applet &
-#run pamac-tray &
-#run xfce4-power-manager &
-#numlockx on &
+# conky -c $HOME/.config/bspwm/system-overview &
+# run variety &
+# run nm-applet &
+# run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
 blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
-#run volumeicon &
-nitrogen --restore &
+run volumeicon &
+#nitrogen --restore &
 #run caffeine &
 #run vivaldi-stable &
 #run firefox &
